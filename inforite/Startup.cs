f59f27017c670f369d446324dbd247b892b30380
Murtuza Khan <mohdmurtuzakhan@gmail.com>
2018-02-10 15:27:36 +0530

    added mvc web project

---
+++ inforite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(inforite.Startup))]
+namespace inforite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
